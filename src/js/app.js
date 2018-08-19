9a1c3798aaa8f3494ff64a439b767cf3b5547918
Aleksandra Bogdanska <alexandra.bogdanska@gmail.com>
2018-08-19 13:40:25 +0200

    new files added

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,67 @@
-(function(){
-    console.log('Witamy na Campie 2018! ');
+(function() {
+  console.log('Witamy na Campie 2018! ');
 
-    const button = document.querySelector("#button");
-    const input = document.querySelector(".clientNo");
-    const result = document.querySelector(".result");
-    const loginFirstStep = document.querySelector("#loginFirstStep");
-    const loginSecondStep = document.querySelector("#loginSecondStep");
-    const closeButton = document.querySelector('.close');
+  const button = document.querySelector("#button");
+  const input = document.querySelector(".clientNo");
+  const result = document.querySelector(".result");
+  const loginFirstStep = document.querySelector("#loginFirstStep");
+  const loginSecondStep = document.querySelector("#loginSecondStep");
+  const closeButton = document.querySelector('.close');
+  const selectOther = document.querySelector('.selectOther');
+  const passwordInput = document.querySelector('.clientPassword');
+  const buttonPassword = document.querySelector('.buttonPassword');
 
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
-      result.innerText = input.value;
-      loginSecondStep.style.display = "block";
-      loginFirstStep.style.display = "none";
-    })
-
-
-    closeButton.addEventListener("click", (event) => {
-      event.preventDefault();
-      loginSecondStep.style.display = "none";
-      loginFirstStep.style.display = "flex";
-      input.value = "";
-    })
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    result.innerText = input.value;
+    loginSecondStep.style.display = "block";
+    loginFirstStep.style.display = "none";
+  })
 
 
+  closeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    loginSecondStep.style.display = "none";
+    loginFirstStep.style.display = "flex";
+    input.value = "";
+  })
+
+  selectOther.addEventListener("click", (event) => {
+    event.preventDefault();
+    loginSecondStep.style.display = "none";
+    loginFirstStep.style.display = "flex";
+    input.value = "";
+  })
+
+
+buttonPassword.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const data = {
+    login: input.value,
+    password: passwordInput.value
+  }
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    data: Qs.stringify(data),
+    url: "https://efigence-camp.herokuapp.com/api/login",
+  };
+
+  // axios(options).then((res) => {
+  //   console.log(res)
+  // });
+
+  axios(options).then(function(response) {
+    console.log('Authenticated');
+    window.location.href = "/dashboard.html";
+  }).catch(function(error) {
+    console.log('Error on Authentication');
+  });
+  });
+
+  
 })();
---
+++ src/js/dashboard.js
@@ -0,0 +1,21 @@
+(function() {
+  console.log('Witamy Dashboard! ');
+
+// const data = {
+//   code: '12345'
+// }
+//
+// const options = {
+//   method: 'POST',
+//   headers: {
+//     'content-type': 'application/x-www-form-urlencoded'
+//   },
+//   data: Qs.stringify(data),
+//   url: "https://efigence-camp.herokuapp.com/api/enter",
+// };
+//
+// axios(options).then((res) => {
+//   console.log(res)
+// });
+
+})();
