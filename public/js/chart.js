df35a08ad4a3f67d04de350909dc8101d09e1093
Aleksandra Bogdanska <alexandra.bogdanska@gmail.com>
2018-08-22 21:58:11 +0200

    chart added

--- public/js/chart.js
+++ public/js/chart.js
@@ -12,9 +12,39 @@ const options = {
 
 axios(options).then(function(response) {
   console.log('Authenticated', response);
+  const data = response.data.content;
+  console.log(data);
+
+  let ctx = document.getElementById("myChart").getContext('2d');
+  let myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: [data[0].date, data[1].date, data[2].date, data[3].date, data[4].date, data[5].date, data[6].date, data[7].date, data[8].date, data[9].date],
+          datasets: [{
+              label: 'income',
+              data: [data[0].amount, data[1].amount, data[2].amount, data[3].amount, data[4].amount, data[5].amount, data[6].amount, data[7].amount, data[8].amount, data[9].amount],
+              backgroundColor: [
+                  'rgba(0, 198, 219, 0.2)',
+              ]
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+
 }).catch(function(error) {
   console.log('Error on Authentication');
 });
+
+
+
 
 
 })();
